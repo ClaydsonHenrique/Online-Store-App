72681f2b57aef65542bcf5110f450420bf7a082a
Luanderson Alves <luaoderson@gmail.com>
2023-03-17 17:47:10 -0300

    feat: feito o componente "Categories" que retorna uma lista de radio-buttons com todas as categorias

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import Categories from './Categories';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-      </div>
+      <>
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
+        <div>
+          <Categories />
+        </div>
+      </>
     );
   }
 }
